acdb4e2b640380186c90f8f396a15b01a9b8e119
Philipp Schuster <phischu3000@googlemail.com>
2016-07-12 13:16:20 +0200

    Notes on conformal model.

--- src/Conformal3D.hs
+++ src/Conformal3D.hs
@@ -1,6 +1,9 @@
 module Conformal3D where
 
 
+import Linear (V3)
+
+
 data Basis = E1 | E2 | E3 | EI | E0
 
 class E1 v where
@@ -15,28 +18,55 @@ class EO v where
 
 -- Entities
 
-data Point a = Point a a a a a
+-- | An element of the representational space with grade one.
+data Vector a = Vector a a a a a
+
+origin :: (Num a) => Vector a
+origin = Vector 1 0 0 0 0
+
+infinity :: (Num a) => Vector a
+infinity = Vector 0 0 0 0 1
+
+euclidean :: (Num a) => V3 a -> Vector a
+euclidean = undefined
+
+-- homogeneous :: Homogeneous.Vector r -> Vector r
+
+-- data Bivector a = ...
+
+-- | A point in 3D space. Could be at infinity. Has a weight.
+data Point a = Point a a a a
 
 data HomogeneousPoint a = HomogeneousPoint a a a a
 
 -- Point pair
 
+-- Sphere
+
+-- PointSphere
+
+
+-- FLATS
+
+-- data FlatPoint r = FlatPoint r r r r
+
 -- Line
 
 -- line :: Point -> Point -> Line
-
 
 -- Circle
 
 -- Plane
 
--- Sphere
 
--- OPERATORS
+-- VERSORS
 
 -- Reflection
 
 -- Inversion
+
+
+-- ROTORS
 
 -- Rotor
 
@@ -55,4 +85,51 @@ data HomogeneousPoint a = HomogeneousPoint a a a a
 -- Motor
 
 -- General Rotor
+
+-- exponential :: Bivector r -> Rotor r
+
+-- logarithm :: Rotor r -> Bivector r
+
+
+-- Properties
+
+-- inverse r == reverse r
+
+-- isAdjoint r (inverse r)
+
+-- isAdjoint f g = forall a b. inner a (g b) == inner (f a) b
+
+
+-- PRODUCTS
+
+-- geometric
+
+-- inner
+
+-- outer
+
+-- contract
+
+-- project
+
+-- reject
+
+-- commutator
+
+-- meet
+
+-- join
+
+-- UNARY OPERATORS
+
+-- reverseVector :: Vector a -> Vector a
+-- reverseBivector :: Bivector a -> Bivector a
+-- reverseRotor
+
+-- inverse
+
+-- dual
+
+-- dualVector
+-- dualSphere
 
---
+++ src/LinearFunction.hs
@@ -0,0 +1,17 @@
+module LinearFunction where
+
+
+
+-- data Linear v w r = Linear (v r -> w r)
+
+-- newtype Orthonormal v w r = ..
+-- Ortho: inner (o x) (o y) = inner x y
+-- Normal: o e = e
+
+-- determinant :: Linear v w r -> r
+-- determinant f = apply f pseudoscalar
+
+-- outermorphism :: Linear v w r -> Linear (Multivector v) (Multivector w) r
+
+
+
