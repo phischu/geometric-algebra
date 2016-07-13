1f92e75bddfcff5cade95f4cf2c3405aa72963a1
Philipp Schuster <phischu3000@googlemail.com>
2016-07-13 18:06:46 +0200

    Add Tutorial module.

---
+++ src/GeometricAlgebra/Tutorial.hs
@@ -0,0 +1,9 @@
+{-| Geometric Algebra unifies different concepts from linear algebra.
+    This tutorial closely follows the motivating example from the book
+    "Geometric Algebra for Computer Science" by Leo Dorst, Daniel Fontijne
+    and Stephen Mann.
+-}
+module GeometricAlgebra.Tutorial where
+
+
+
