ce9879d0798301be80d7bbc1e3986386f8cc950d
Philipp Schuster <phischu3000@googlemail.com>
2016-07-29 15:33:34 +0200

    Dual vectors.

--- src/GeometricAlgebra/LinearFunction.hs
+++ src/GeometricAlgebra/LinearFunction.hs
@@ -1,6 +1,22 @@
 module GeometricAlgebra.LinearFunction where
 
+import Linear (
+  Metric(dot),
+  identity)
 
+import Data.Traversable (
+  Traversable)
+import Control.Applicative (
+  Applicative)
+
+
+newtype Dual v r = Dual { unDual :: v r -> r }
+
+toDual :: (Metric v, Num r) => v r -> Dual v r
+toDual x = Dual (dot x)
+
+fromDual :: (Applicative v, Traversable v, Num r) => Dual v r -> v r
+fromDual x = fmap (unDual x) identity
 
 -- data Linear v w r = Linear (v r -> w r)
 
